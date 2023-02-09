ffdc768c77052f9825c7fa9478506fbe6e71c3c0
Bekhzod Akbarov <stx.webdev@gmail.com>
2023-02-09 18:23:11 +0500

    create <Category /> page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/Navbar';
 import PrivateRoute from './components/PrivateRoute';
+import Category from './pages/Category';
 import Explore from './pages/Explore';
 import ForgotPassword from './pages/ForgotPassword';
 import Offers from './pages/Offers';
@@ -17,6 +18,8 @@ export default function App() {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
+          <Route path='/category/:categoryName' element={<Category />} />
+
           <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
           </Route>
